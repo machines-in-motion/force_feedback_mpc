only set friction constraints when 3d force in IAM + fixed sign of force ref plot + removed hack on collision lower bound in soft polishing obstacle

demos/polishing/soft/polishing_soft_mpc_obstacle.py
@@ -213,23 +213,26 @@ for k,m in enumerate(models):
     m.differential.ref = pin.LOCAL_WORLD_ALIGNED
     m.differential.costs.costs['rotation'].active = False
     m.differential.costs.costs['rotation'].cost.residual.reference = pin.utils.rpyToMatrix(np.pi, 0., np.pi)
-    # if(k!=0):
-    # m.force_lb =  np.array([-10000.])
-    m.with_force_constraint = False
-        # m.force_lb = np.array([-10000.])
-        # m.force_ub = np.array([10000.])
-    # m.force_ub =  np.array([10000.])
-    # # set each collision constraint bounds to [0, inf]
-    # for col_idx in range(len(robot.collision_model.collisionPairs)):
-    #     # only populates the bounds of the constraint item (not the manager)
-    #     m.differential.constraints.constraints['collisionBox_' + str(col_idx)].constraint.updateBounds(
-    #                 np.array([0.]),
-    #                 np.array([np.inf])) 
-    #     # needed to pass the bounds to the manager
-    #     m.differential.constraints.changeConstraintStatus('collisionBox_' + str(col_idx), True)
-    #     # need to set explicitly the IAM bounds
-    #     m.g_lb = -0.0001*np.ones([m.ng]) # needs to be slightly negative (bug to investigate)
-    #     m.g_ub = np.array([np.inf]*m.ng)
+    # FORCE CONSTRAINT STUFF
+    # # if(k!=0):
+    # # m.force_lb =  np.array([-10000.])
+    # m.with_force_constraint = True
+    #     # m.force_lb = np.array([-10000.])
+    #     # m.force_ub = np.array([10000.])
+    # # m.force_ub =  np.array([10000.])
+    # COLLISION CONSTRAINT STUFF
+    # set each collision constraint bounds to [0, inf]
+    for col_idx in range(len(robot.collision_model.collisionPairs)):
+        # only populates the bounds of the constraint item (not the manager)
+        m.differential.constraints.constraints['collisionBox_' + str(col_idx)].constraint.updateBounds(
+                    np.array([0.]),
+                    np.array([np.inf])) 
+        # needed to pass the bounds to the manager
+        m.differential.constraints.changeConstraintStatus('collisionBox_' + str(col_idx), True)
+        # need to set explicitly the IAM bounds
+        # m.g_lb = -0.0001*np.ones([m.ng]) # needs to be slightly negative (bug to investigate)
+        m.g_lb = np.zeros([m.ng]) # needs to be slightly negative (bug to investigate)
+        m.g_ub = np.array([np.inf]*m.ng)
 # wfhwef
 # solver.setCallbacks([mim_solvers.CallbackVerbose(), mim_solvers.CallbackLogger()])
 solver.solve(xs_init, us_init, maxiter=100, isFeasible=False)