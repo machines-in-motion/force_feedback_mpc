fixed force ref sign

python/soft_mpc/Go2MPC_demo.py
@@ -32,13 +32,28 @@ robot.updateHeightMap(map)
 # Instantiate the solver
 assets_path = '/home/skleff/force_feedback_ws/Go2Py/Go2Py/assets/'
 MU = 0.75
-mpc = Go2MPC(assets_path, HORIZON=5, friction_mu=MU, dt=0.005)
+mpc = Go2MPC(assets_path, HORIZON=10, friction_mu=MU, dt=0.02)
 mpc.initialize()
 mpc.max_iterations=100
 mpc.solve()
 m = list(mpc.solver.problem.runningModels) + [mpc.solver.problem.terminalModel]
 
 # plot_ocp_solution_with_cones(mpc)
+# Extract OCP Solution 
+force_sol = []
+xs, us = mpc.solver.xs, mpc.solver.us
+for time_idx in range (mpc.HORIZON+1):
+    f = xs[time_idx][-3:]
+    force_sol    += [f]
+force_sol = np.array(force_sol)   
+time_span = np.linspace(0, mpc.HORIZON*mpc.dt, mpc.HORIZON+1)
+import matplotlib.pyplot as plt
+plt.plot(time_span, force_sol[:,0], label='x')
+plt.plot(time_span, [-15]*(mpc.HORIZON+1), 'k-.', label='ref x')
+plt.plot(time_span, force_sol[:,1], label='y')
+plt.plot(time_span, force_sol[:,2], label='z')
+plt.legend()
+plt.show()
 
 # Reset the robot
 state = mpc.getSolution(0)
@@ -50,7 +65,7 @@ robot.reset()
 # Solve for as many iterations as needed for the first step
 mpc.max_iterations=10
 
-Nsim = 500
+Nsim = 1000
 # measured_forces = []
 measured_forces_dict = {}
 predicted_forces_dict = {}
@@ -91,6 +106,10 @@ setGroundFriction(robot.model, robot.data, MU)
 # force_est_butter2 = np.zeros(3)
 # force_est_butter2_ = [] 
 
+MPC_FREQ    = 100
+SIM_FREQ    = int(1./robot.dt)
+
+
 # Main simulation loop
 f_mea_all = np.zeros(3*5)
 for i in range(Nsim):
@@ -107,16 +126,18 @@ for i in range(Nsim):
     omega = robot.data.qvel[3:6]
     q = np.hstack([q, np.zeros(2)])
     dq = np.hstack([dq, np.zeros(2)])
-    # Solve OCP
+    # Measure forces
     for fname in mpc.ee_frame_names:
         f_mea = -getForceSensor(robot.model, robot.data, frame_name_to_mujoco_sensor[fname]).squeeze().copy()
-        # if(fname =='Link6'):
+        # if(fname !='Link6'):
         #     print("filter")
         #     # Filter force using butterworth LPF
         #     force_est_butter2[0] = butter2_Fx_ee.filter(f_mea[0])
         #     force_est_butter2[1] = butter2_Fy_ee.filter(f_mea[1])
         #     force_est_butter2[2] = butter2_Fz_ee.filter(f_mea[2])
-        #     print(f_mea)
+            # print(fname, " : ", f_mea)
+            # if(MU * f_mea[2] - np.sqrt(f_mea[0]*f_mea[0] + f_mea[1]*f_mea[1]) > 0):
+            #     print(fname, " is slipping !")
         #     print(force_est_butter2)
         #     force_est_butter2_.append(force_est_butter2)
         #     # apply filter on feedback
@@ -127,8 +148,11 @@ for i in range(Nsim):
 
         # print("Extract name ", fname, " , Mujoco sensor = ", frame_name_to_mujoco_sensor[fname], " id = ", id_f, " sol name = ", frame_name_to_sol_map[fname] )
         # print("value = ", f_mea)
-    if(i%2 == 0):
+    # Solve OCP
+    if(i%int(SIM_FREQ/MPC_FREQ)==0):
         solution = mpc.updateAndSolve(t, quat, q, v, omega, dq, f_mea_all)
+        # if(mpc.solver.KKT > 1e-4):
+        #     fheor
     for fname in mpc.ee_frame_names:
         predicted_forces_dict[fname].append(solution[frame_name_to_sol_map[fname]])
     # Save the solution

python/soft_mpc/Go2MPC_wrapper.py
@@ -101,7 +101,7 @@ def plot_ocp_solution_with_cones(mpc):
     force_sol = []
     xs, us = solver.xs, solver.us
     x = []
-    for time_idx in range (N):
+    for time_idx in range (T):
         q, v = xs[time_idx][:nq], xs[time_idx][nq:nq+nv]
         f = xs[time_idx][nq+nv:]
         pin.framesForwardKinematics(rmodel, rdata, q)
@@ -115,7 +115,7 @@ def plot_ocp_solution_with_cones(mpc):
         jointVel_sol += [v]
         force_sol    += [f]
         x += [xs[time_idx]]
-        if time_idx < N-1:
+        if time_idx < T-1:
             jointAcc_sol +=  [solver.problem.runningDatas[time_idx].xnext[nq::]] 
             jointTorques_sol += [us[time_idx]]
 
@@ -124,16 +124,16 @@ def plot_ocp_solution_with_cones(mpc):
                         'jointTorques':jointTorques_sol}       
 
     # Extract contact forces by hand
-    sol['FL_FOOT_contact'] = [force_sol[i][0:3] for i in range(N)]     
-    sol['FR_FOOT_contact'] = [force_sol[i][3:6] for i in range(N)]     
-    sol['HL_FOOT_contact'] = [force_sol[i][6:9] for i in range(N)]     
-    sol['HR_FOOT_contact'] = [force_sol[i][9:12] for i in range(N)]     
-    sol['Link6'] = [force_sol[i][12:15] for i in range(N)]     
+    sol['FL_FOOT_contact'] = [force_sol[i][0:3] for i in range(T)]     
+    sol['FR_FOOT_contact'] = [force_sol[i][3:6] for i in range(T)]     
+    sol['HL_FOOT_contact'] = [force_sol[i][6:9] for i in range(T)]     
+    sol['HR_FOOT_contact'] = [force_sol[i][9:12] for i in range(T)]     
+    sol['Link6'] = [force_sol[i][12:15] for i in range(T)]     
 
     # Plotting 
     import matplotlib.pyplot as plt
     constrained_sol = sol
-    time_lin = np.linspace(0, T, solver.problem.T+1)
+    time_lin = np.linspace(0, T, T)
     fig, axs = plt.subplots(4, 3, constrained_layout=True)
     for i, frame_idx in enumerate(supportFeetIds):
         ct_frame_name = rmodel.frames[frame_idx].name + "_contact"
@@ -157,9 +157,19 @@ def plot_ocp_solution_with_cones(mpc):
     axs[3, 0].set_xlabel(r"$F_x$")
     axs[3, 1].set_xlabel(r"$F_y$")
     axs[3, 2].set_xlabel(r"$F_z$")
-    fig.suptitle('Force', fontsize=16)
+    fig.suptitle('Force feet', fontsize=16)
 
+    fig, axs = plt.subplots(1, 3, constrained_layout=True)
+    forces = np.array(constrained_sol['Link6'])
+    axs[0].plot(time_lin, forces[:, 0], label="Fx")
+    axs[1].plot(time_lin, forces[:, 1], label="Fy")
+    axs[2].plot(time_lin, forces[:, 2], label="Fz")
+    axs[0].legend()
+    fig.suptitle('Force EE', fontsize=16)
 
+    axs[0].set_xlabel(r"$F_x$")
+    axs[1].set_xlabel(r"$F_y$")
+    axs[2].set_xlabel(r"$F_z$")
     # comDes = np.array(comDes)
     # centroidal_sol = np.array(constrained_sol['centroidal'])
     # plt.figure()
@@ -310,11 +320,15 @@ class Go2MPC:
             stateResidual = crocoddyl.ResidualModelState(self.ccdyl_state, self.x0, self.nu)
             stateActivation = crocoddyl.ActivationModelWeightedQuad(stateWeights**2)
             stateReg = crocoddyl.CostModelResidual(self.ccdyl_state, stateActivation, stateResidual)
-            costModel.addCost("stateReg", stateReg, state_reg_weight)
+            # if t != self.HORIZON:
+            costModel.addCost("stateReg", stateReg, state_reg_weight*0.1)
+            # else:
+            #     costModel.addCost("stateReg", stateReg, state_reg_weight*self.dt)
+                
             if t != self.HORIZON:
                 ctrlResidual = crocoddyl.ResidualModelControl(self.ccdyl_state, self.nu)
                 ctrlReg = crocoddyl.CostModelResidual(self.ccdyl_state, ctrlResidual)
-                costModel.addCost("ctrlReg", ctrlReg, control_reg_weight)  
+                costModel.addCost("ctrlReg", ctrlReg, control_reg_weight*0.1)  
 
             # Body COM Tracking Cost
             # com_residual = crocoddyl.ResidualModelCoMPosition(self.ccdyl_state, comRef, self.nu)
@@ -351,7 +365,13 @@ class Go2MPC:
             constraintModelManager = None #crocoddyl.ConstraintModelManager(self.ccdyl_state, self.nu)
 
             # Custom force cost in DAM
-            forceCostManager = ForceCostManager([ForceCost(self.ccdyl_state, self.armEEId, np.array([-15, 0., 0.]), 0.1, pin.LOCAL_WORLD_ALIGNED)], softContactModelsStack)
+            f_weight = 0.01
+            if t != self.HORIZON:
+                forceCostEE = ForceCost(self.ccdyl_state, self.armEEId, np.array([-15, 0., 0.]), f_weight, pin.LOCAL_WORLD_ALIGNED)
+            else:
+                forceCostEE = ForceCost(self.ccdyl_state, self.armEEId, np.array([-15, 0., 0.]), f_weight*self.dt, pin.LOCAL_WORLD_ALIGNED)
+
+            forceCostManager = ForceCostManager([forceCostEE], softContactModelsStack)
 
             # Create DAM with soft contact models, force costs + standard cost & constraints
             dam = DAMSoftContactDynamics3D_Go2(self.ccdyl_state, self.ccdyl_actuation, costModel, softContactModelsStack, constraintModelManager, forceCostManager)
@@ -399,15 +419,16 @@ class Go2MPC:
         # solver.eps_abs = 1e-6
         # solver.eps_rel = 0.
 
-        solver.max_qp_iters = 1000
+        solver.max_qp_iters = 10000
         solver.with_callbacks = True
         solver.use_filter_line_search = False
-        solver.mu_constraint = -1
-        solver.lag_mul_inf_norm_coef = 10.
-        solver.termination_tolerance = 1e-2
+        solver.mu_constraint = -1 #1e-4 #-3
+        # solver.mu_dynamic = 1e2 #-1
+        # solver.lag_mul_inf_norm_coef = 2.
+        solver.termination_tolerance = 1e-4
         solver.eps_abs = 1e-6
         solver.eps_rel = 1e-6
-
+        # solver.extra_iteration_for_last_kkt = True
         self.solver = solver
 
     def getSolution(self, k=None):

python/soft_mpc/soft_multicontact_api.py
@@ -40,11 +40,11 @@ class ViscoElasticContact3D:
         self.fout      = np.zeros(self.nc)
         self.fout_copy = np.zeros(self.nc)
         # Partial derivatives
-        self.dABA_df = np.zeros((self.state.nv, self.nc))
-        self.dfdt_dx   = np.zeros((self.nc, self.state.ndx))
-        self.dfdt_du   = np.zeros((self.nc, self.actuation.nu))
-        self.dfdt_df   = np.zeros((self.nc, self.nc))  
-        
+        self.dABA_df3d = np.zeros((self.state.nv, self.nc))    # derivative of ada (ddq) w.r.t. 3d force
+        self.dfdt_dx = np.zeros((self.nc, self.state.ndx))     # derivative of fdot (3D) w.r.t. state 
+        self.dfdt_du = np.zeros((self.nc, self.actuation.nu))  # derivative of fdot (3D) w.r.t. control
+        self.dfdt_df = np.zeros((self.nc, self.nc))            # derivative of fdot (3D) w.r.t. forces
+
     def calc(self, dad, f):
         '''
         Calculate the contact force spatial wrench 
@@ -81,17 +81,18 @@ class ViscoElasticContact3D:
         # Compute derivatives of data.xout (ABA) w.r.t. f in LOCAL 
         lJ = pin.getFrameJacobian(self.pinocchio, dad.pinocchio, self.frameId, pin.LOCAL)  
         oRf = dad.pinocchio.oMf[self.frameId].rotation
-        self.dABA_df = dad.pinocchio.Minv @ lJ[:3].T  
+        self.dABA_df3d = dad.pinocchio.Minv @ lJ[:3].T  
         # Skew term added to RNEA derivatives when force is expressed in LWA
         if(self.pinRef != pin.LOCAL):
             # logger.debug("corrective term aba LWA : \n"+str(data.pinocchio.Minv @ lJ[:3].T @ pin.skew(oRf.T @ f) @ lJ[3:]))
             dad.Fx[:,:self.state.nv] += dad.pinocchio.Minv @ lJ[:3].T @ pin.skew(oRf.T @ f) @ lJ[3:]
             # Rotate dABA/df
-            self.dABA_df = self.dABA_df @ oRf.T 
+            self.dABA_df3d = self.dABA_df3d @ oRf.T 
 
     def calcDiff(self, dad):  
         '''
         Compute partial derivatives of the soft contact force time derivative w.r.t. state and input
+        must be called after update_ABAderivatives
         '''
         oRf = dad.pinocchio.oMf[self.frameId].rotation
         # Derivatives of data.fout in LOCAL : important >> UPDATE FORWARD KINEMATICS with data.xout
@@ -102,7 +103,7 @@ class ViscoElasticContact3D:
         da_dx[:,:self.state.nv] = a_dq[:3] + a_da[:3] @ dad.Fx[:,:self.state.nv] # same as aba_dq here
         da_dx[:,self.state.nv:] = a_dv[:3] + a_da[:3] @ dad.Fx[:,self.state.nv:] # same as aba_dv here
         da_du = a_da[:3] @ dad.Fu
-        da_df = a_da[:3] @ dad.dABA_df
+        da_df = a_da[:3] @ dad.dABA_df # this dad attribute was computed in update update_ABAderivatives
         # Deriv of lambda dot
         self.dfdt_dx = -self.Kp*lv_dx[:3] - self.Kv*da_dx[:3]
         self.dfdt_du = -self.Kv*da_du
@@ -117,7 +118,7 @@ class ViscoElasticContact3D:
             self.dfdt_dx[:,self.state.nv:] = oRf @ self.ldfdt_dx_copy[:,self.state.nv:] 
             self.dfdt_du = oRf @ self.ldfdt_du_copy
             self.dfdt_df = oRf @ self.ldfdt_df_copy
-            
+
 
 # 3D Soft contact models stack
 class ViscoElasticContact3d_Multiple:
@@ -179,7 +180,7 @@ class ViscoElasticContact3d_Multiple:
         for ct in self.contacts:
             if(ct.active):
                 ct.update_ABAderivatives(dad, f[nc_i:nc_i+ct.nc])
-                dad.dABA_df[0:self.nv, nc_i:nc_i+ct.nc] = ct.dABA_df
+                dad.dABA_df[0:self.nv, nc_i:nc_i+ct.nc] = ct.dABA_df3d
             nc_i += ct.nc
     
     def calcDiff(self, dad):
@@ -336,7 +337,7 @@ class ForceCost:
         self.frameId       = frameId
         self.state         = state
         self.f_des         = f_des
-        self.f_weight      = np.diag([f_weight]*3)
+        self.f_weight      = np.diag([f_weight,0.,0.]) # np.diag([f_weight]*3) # 
         self.f_residual    = np.zeros(3)
         self.f_residual_x  = np.zeros((3,state.ndx))
         self.f_cost        = 0.
@@ -445,7 +446,6 @@ class ForceCostManager:
             nr_i   = 0
             # For each constraint active at this model
             for cost in self.contact_to_cost_map[ct.frameId]:
-                # Compute the constraint residual
                 self.cost += cost.calc(dad, f[nc_i:nc_i+ct.nc], pinRefDyn)
                 nr_i += cost.nr
             nc_i += ct.nc
@@ -467,7 +467,6 @@ class ForceCostManager:
             nr_i   = 0
             # For each constraint active at this model
             for cost in self.contact_to_cost_map[ct.frameId]:
-                # Compute the constraint residual
                 Lf_ct, Lff_ct = cost.calcDiff(dad, f[nc_i:nc_i+ct.nc], pinRefDyn)
                 self.Lf[nc_i:nc_i+cost.nc] = Lf_ct
                 self.Lff[nr_i:nr_i+cost.nr, nc_i:nc_i+cost.nc] = Lff_ct

python/soft_mpc/test_soft_multicontact_go2.py
@@ -145,7 +145,7 @@ for t in range(N_ocp+1):
     if t == N_ocp:
         forceCostManager = ForceCostManager([ ForceCost(state, efId, np.array([-25, 0., 0.]), 1e-3*dt, pinRef) ], softContactModelsStack)
     else:
-        forceCostManager = ForceCostManager([ ForceCost(state, efId, np.array([-25, 0., 0.]), 1e-3, pinRef) ], softContactModelsStack)
+        forceCostManager = ForceCostManager([ ForceCost(state, efId, np.array([-25, 0., 0.]), 1e-3, pinRef, fdotReg=0.01) ], softContactModelsStack)
 
     # Create DAM with soft contact models, force costs + standard cost & constraints
     dam = DAMSoftContactDynamics3D_Go2(state, actuation, costModel, softContactModelsStack, constraintModelManager, forceCostManager)
@@ -318,9 +318,11 @@ if(TESTING_API):
             v[iv] = 0.0
         return np.array(Fq).T
 
-    def numdiff_x_iam(f, x0, state, h=1e-6):
+
+    def numdiff_x_iam_dyn(f, x0, state, h=1e-6):
         '''
         Numdiff in tangent space for Integrated Action models
+        partial of dynamics (xnext) w.r.t. state x
         '''
         f0 = f(x0).copy()
         x = x0.copy()
@@ -338,10 +340,10 @@ if(TESTING_API):
             dx[idx] = 0.0
         return np.array(Fx).T
 
-
-    def numdiff_u_iam(f, u0, state, h=1e-6):
+    def numdiff_u_iam_dyn(f, u0, state, h=1e-6):
         '''
         Numdiff in tangent space for Integrated Action models
+        partial of dynamics (xnext) w.r.t. control u
         '''
         f0 = f(u0).copy()
         u = u0.copy()
@@ -353,6 +355,46 @@ if(TESTING_API):
             u[idu] += h
             # Compute finite difference
             Fu.append(state.diff(f0, f(u)) / h)
+            # Reset perturbation
+            u[idu] -= h
+        return np.array(Fu).T
+
+
+    def numdiff_x_iam_cost(f, x0, state, h=1e-6):
+        '''
+        Numdiff in tangent space for Integrated Action models
+        partial of cost (scalar) w.r.t. control u
+        '''
+        f0 = f(x0)
+        x = x0.copy()
+        # Create perturbation vector
+        dx = np.zeros(state.ndx)
+        Fx = []
+        for idx in range(state.ndx):
+            # Apply perturbation to the iv-th component
+            dx[idx] = h
+            x = state.integrate(x0, dx)
+            # Compute finite difference
+            Fx.append( ( f(x) - f0 )/ h)
+            # Reset perturbation
+            dx[idx] = 0.0
+        return np.array(Fx).T
+
+    def numdiff_u_iam_cost(f, x0, state, h=1e-6):
+        '''
+        Numdiff in tangent space for Integrated Action models
+        partial of cost (scalar) w.r.t. control u
+        '''
+        f0 = f(u0)
+        u = u0.copy()
+        # Create perturbation vector
+        du = np.zeros_like(u0) 
+        Fu = []
+        for idu in range(len(du)):
+            # Apply perturbation to the iv-th component
+            u[idu] += h
+            # Compute finite difference
+            Fu.append( ( f(u) - f0 )/ h)
             # Reset perturbation
             u[idu] -= h
         return np.array(Fu).T
@@ -374,7 +416,7 @@ if(TESTING_API):
     dfdot_du = dad.dfdt_du
 
     # Compute integral action model derivatives
-    iam = runningModels[0]
+    iam = runningModels[10]
     iad = iam.createData()
     iam.calc(iad, y, u)
     iam.calcDiff(iad, y, u)
@@ -403,8 +445,7 @@ if(TESTING_API):
         iam.calc(iad, y, u)
         return iad.xnext 
     
-    def get_iam_cost(iam, iad, q, v, f, u):
-        y = np.concatenate([q,v,f])
+    def get_iam_cost(iam, iad, y, u):
         iam.calc(iad, y, u)
         return iad.cost
 
@@ -417,10 +458,10 @@ if(TESTING_API):
     dfdot_df_ND = numdiff(lambda f_:get_fdot(dam, dad, q, v, f_, u), f)
     dfdot_du_ND = numdiff(lambda u_:get_fdot(dam, dad, q, v, f, u_), u)
 
-    dynext_dx_ND = numdiff_x_iam(lambda y_:get_ynext_y(iam, iad, y_, u), y, iam.stateSoft)
-    dynext_du_ND = numdiff_u_iam(lambda u_:get_ynext_y(iam, iad, y, u_), u, iam.stateSoft)
-    # dcost_dy_ND = numdiff(lambda y_:get_iam_cost(iam, iad, y_, u), y)
-    # dcost_du_ND = numdiff(lambda u_:get_iam_cost(iam, iad, y, u_), u)
+    dynext_dx_ND = numdiff_x_iam_dyn(lambda y_:get_ynext_y(iam, iad, y_, u), y, iam.stateSoft)
+    dynext_du_ND = numdiff_u_iam_dyn(lambda u_:get_ynext_y(iam, iad, y, u_), u, iam.stateSoft)
+    dcost_dy_ND = numdiff_x_iam_cost(lambda y_:get_iam_cost(iam, iad, y_, u), y, iam.stateSoft)
+    dcost_du_ND = numdiff_u_iam_cost(lambda u_:get_iam_cost(iam, iad, y, u_), u, iam.stateSoft)
     
     assert(norm(dxdot_dq - dxdot_dq_ND) <= TOL)
     assert(norm(dxdot_dv - dxdot_dv_ND) <= TOL)
@@ -432,5 +473,6 @@ if(TESTING_API):
     assert(norm(dfdot_du - dfdot_du_ND) <= TOL)
     assert(norm(dynext_dx - dynext_dx_ND) <= TOL)
     assert(norm(dynext_du - dynext_du_ND) <= TOL)
-    # assert(norm(dcost_dy - dcost_dy_ND) <= TOL)
-    # assert(norm(dcost_du - dcost_dy_ND) <= TOL)
+    assert(norm(dcost_dy - dcost_dy_ND) <= TOL)
+    assert(norm(dcost_du - dcost_du_ND) <= TOL)
+    print("\n---> ALL TESTS PASSED.\n")