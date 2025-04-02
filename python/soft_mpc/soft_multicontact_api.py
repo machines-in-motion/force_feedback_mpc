fixed unittest DAM.Lq by reducing the base orientation weigth in Go2 unittest + fixed derivative of friction cone when force is zero

python/soft_mpc/soft_multicontact_api.py
@@ -221,7 +221,7 @@ class FrictionConeConstraint:
             self.residual_df[:, 1] = -f[1] / np.sqrt(f[0]*f[0] + f[1]*f[1])
             self.residual_df[:, 2] = self.coef
         else:
-            self.residual_df = np.zeros((self.nr, self.nc))
+            self.residual_df = np.array([[-1, -1, self.coef]])
         return self.residual_df
 
 