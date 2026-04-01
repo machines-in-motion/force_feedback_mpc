3040fc44b54441c32c312d4530f24be46a237836
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-02 00:54:02 +0200

    fix pinocchio 3 use

--- tests/factory/state-soft.cpp
+++ tests/factory/state-soft.cpp
@@ -14,7 +14,7 @@
 #include <crocoddyl/multibody/states/multibody.hpp>
 #include <example-robot-data/path.hpp>
 #include <pinocchio/fwd.hpp>
-#include <pinocchio/parsers/sample-models.hpp>
+#include <pinocchio/multibody/sample-models.hpp>
 #include <pinocchio/parsers/urdf.hpp>
 
 #include "state-soft.hpp"
